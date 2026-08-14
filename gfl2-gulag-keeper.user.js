362f491fd8aaf2d8dec0ac13044c720b51eb9347
jp-stagger-collab <staggerhans2@gmail.com>
2026-08-14 20:01:56 +0900

    Update gfl2-gulag-keeper.user.js

--- gfl2-gulag-keeper.user.js
+++ gfl2-gulag-keeper.user.js
@@ -1261,7 +1261,7 @@
 
     const title = document.createElement('div');
     title.className = 'acbm-title';
-    title.textContent = '솦챈 굴라그 키퍼 v0.4.2';
+    title.textContent = '솦챈 굴라그 키퍼 v0.4.4';
 
     const status = document.createElement('div');
     status.className = 'acbm-row';
